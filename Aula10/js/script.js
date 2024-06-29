6fd29595b58b8b1218ef163aa7debc2e1423c94f
Felipe Gabriel <gabrielfelipealves.gfa@gmail.com>
2024-06-29 09:52:19 -0300

    Aula 10 - API TheMovieDB Alteração Layout página

--- Aula10/js/script.js
+++ Aula10/js/script.js
@@ -17,39 +17,90 @@ O fetch é uma promise
 
 
 fetch("https://api.themoviedb.org/3/movie/popular?language=pt-BR&page=1&api_key=792b567e08e5221da96e64aa49663f8b")
-.then((response)=>response.json())
-.then((dados)=>{
-   
-    dados.results.map((filme)=>{
-        let cartaz = document.createElement("div");
-        cartaz.setAttribute("class","cartaz");
-        let poster = document.createElement("img");
-        poster.src=img+filme.poster_path;
-        
-        let votos = document.createElement("div");
-        votos.setAttribute("class","votos")
-        votos.innerHTML = filme.vote_average;
+    .then((response) => response.json())
+    .then((dados) => {
 
-        let titulo = document.createElement("h2");
-        titulo.innerHTML = filme.title;
+        dados.results.map((filme) => {
+            let cartaz = document.createElement("div");
+            cartaz.setAttribute("class", "cartaz");
+            let poster = document.createElement("img");
+            poster.src = img + filme.poster_path;
 
-        let lancamento = document.createElement("p");
-        lancamento.setAttribute("class", "lancamento");
-        lancamento.innerHTML=filme.release_date;
-        
-        
-        
-        
-        cartaz.appendChild(poster);
-        cartaz.appendChild(votos);
-        cartaz.appendChild(titulo);
-        cartaz.appendChild(lancamento);
+            let votos = document.createElement("div");
+            votos.setAttribute("class", "votos")
+            votos.innerHTML = Math.round(filme.vote_average) + "%";
 
-        estrutura.appendChild(cartaz);
+            let titulo = document.createElement("h2");
+            titulo.innerHTML = filme.title;
+
+            let lancamento = document.createElement("p");
+            lancamento.setAttribute("class", "lancamento");
+            /*Vamos utilizar o comando chamado substring, que
+            nos ajuda a quebrar uma string com base na
+            posição do caracter e a quantidade de caracteres
+            Por exemplo: 2024-06-13. Para pegar os quatro primeiros dígitos usamos:
+            substring(0,4) 
+            */
+            let ano = filme.release_date.substring(0, 4);
+            let mes = filme.release_date.substring(5, 7);
+            let dia = filme.release_date.substring(8, 10);
+
+            switch(mes){
+                case "01":
+                    mes="jan";
+                    break;
+                case "02":
+                    mes="fev";
+                    break;  
+                case "03":
+                    mes="mar";
+                    break;
+                case "04":
+                    mes="abr";
+                    break;
+                case "05":
+                    mes="mai";
+                    break; 
+                case "06":
+                    mes="jun";
+                    break;  
+                case "07":
+                    mes="jul";
+                    break; 
+                case "08":
+                    mes="ago";
+                    break;  
+                case "09":
+                    mes="set";
+                    break; 
+                case "10":
+                    mes="out";
+                    break;  
+                case "11":
+                    mes="nov";
+                    break;  
+                default:
+                    mes="dez";
+                    break
+
+            }
+
+
+            lancamento.innerHTML = `${dia} de ${mes} de ${ano}`;
+
+
+
+
+            cartaz.appendChild(poster);
+            cartaz.appendChild(votos);
+            cartaz.appendChild(titulo);
+            cartaz.appendChild(lancamento);
+
+            estrutura.appendChild(cartaz);
+        })
+
     })
-
-})
-.catch((erro)=>console.error(erro));
+    .catch((erro) => console.error(erro));
 
 
 
